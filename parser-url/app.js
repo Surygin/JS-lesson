a9fc32596ac9bb16306e37a1a51441e111a513f3
Anton Surygin <surygin@live.com>
2023-08-07 23:07:08 +0300

    доработал скрипт

--- parser-url/app.js
+++ parser-url/app.js
@@ -1,4 +1,4 @@
-const url = 'https://stepik.org/course/63054/promo';
+const url = 'http://stepik.org/course/63054/promo';
 
 const splitUrl = url.split('/');
 
@@ -6,7 +6,7 @@ const [protocolType] = splitUrl[0].split(':');
 
 const [protocol, _, domen, ...otherData] = url.split('/');
 
-console.log(`Протокол - ${[protocolType]} == 'http' ? 'не безопасное подключение' : '',
-Доменное имя - ${[domen]},
-Путь внутри сайта - ${[otherData.join('/')]}.
+console.log(`Протокол: ${[protocolType] == 'http' ? 'http - не безопасное подключение' : 'https' },
+Доменное имя: ${[domen]},
+Путь внутри сайта: ${[otherData.join('/')]}.
 `);
