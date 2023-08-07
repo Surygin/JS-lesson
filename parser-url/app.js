1a09a876524f1933039a134c7118ee03d25161a9
Anton Surygin <surygin@live.com>
2023-08-07 22:54:35 +0300

    Сделал URL парсер

---
+++ parser-url/app.js
@@ -0,0 +1,12 @@
+const url = 'https://stepik.org/course/63054/promo';
+
+const splitUrl = url.split('/');
+
+const [protocolType] = splitUrl[0].split(':');
+
+const [protocol, _, domen, ...otherData] = url.split('/');
+
+console.log(`Протокол - ${[protocolType]},
+Доменное имя - ${[domen]},
+Путь внутри сайта - ${[otherData.join('/')]}.
+`);
