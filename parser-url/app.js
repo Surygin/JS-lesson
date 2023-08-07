8106898336e9b2a62e49617a4b0d9d85f23ebf58
Anton Surygin <surygin@live.com>
2023-08-07 22:59:53 +0300

    тестирую тернарный оператор

--- parser-url/app.js
+++ parser-url/app.js
@@ -6,7 +6,7 @@ const [protocolType] = splitUrl[0].split(':');
 
 const [protocol, _, domen, ...otherData] = url.split('/');
 
-console.log(`Протокол - ${[protocolType]},
+console.log(`Протокол - ${[protocolType]} == 'http' ? 'не безопасное подключение' : '',
 Доменное имя - ${[domen]},
 Путь внутри сайта - ${[otherData.join('/')]}.
 `);
