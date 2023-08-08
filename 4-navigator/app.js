99df3924fc7119e40ef0e023250dfdd0f723a326
Anton Surygin <surygin@live.com>
2023-08-08 23:41:14 +0300

    Доработал навигатор и убрал лишние файлы

--- 4-navigator/app.js
+++ 4-navigator/app.js
@@ -8,20 +8,22 @@ const positionLong = Math.random();
 
 // Расчет растояния
 const distanceLat = addressLat - positionLat;
+const distanceLatReverse = positionLat - addressLat;
 const distanceLong = addressLong - positionLong;
+const distanceLongReverse =  positionLong - addressLong;
 
 // Расчет дистанции
 if (distanceLat < 0){
-    const msg = 'Пользователь Lat ушел дальше конечной точки!';
-    console.log(msg);
+    console.log(`Пользователю Lat 
+        необходио вернутся на ${distanceLatReverse}, к конечной точке.`);
 } else {
     console.log(`Пользователю Lat 
         необходио преодолеть еще ${distanceLat}, до конечной точки.`);
 }
 
 if (distanceLong < 0){
-    const msg = 'Пользователь Long ушел дальше конечной точки!';
-    console.log(msg);
+    console.log(`Пользователю Long 
+        необходио вернутся на ${distanceLongReverse}, к конечной точке.`);
 } else {
     console.log(`Пользователю Long
         необходио преодолеть еще ${distanceLong}, до конечной точки.`);
