2336f62db2ccfd6012459e5bf74ddc32bce8b692
Anton Surygin <surygin@live.com>
2023-08-22 09:48:14 +0300

    переписал через деструктуризацию - если я правильно это понял

--- 9-sort-loops/app.js
+++ 9-sort-loops/app.js
@@ -1,18 +1,28 @@
 const numbers = [1, 40, -5, 10, 0];
 
-function sort(arr){
+function sortArray(arr){
     for(let el in arr){
         for (let item in arr){
-            let num1 = arr[el];
-            let num2 = arr[item];
+            const [num1, num2] = [arr[el], arr[item]];
             if (arr[el] < arr[item]){
-                arr[item] = num1;
-                arr[el] = num2;
-
+                [arr[item], arr[el]] = [num1, num2]
             }
         }
     }
     console.log(arr);
 }
 
-sort(numbers);
+sortArray(numbers);
+
+// function sortArray(arr){
+//     for(let el of arr){
+//         for (let item of arr){
+//             const [num1, num2] = [el, item];
+//             // console.log([num1, num2])
+//             if (el < item){
+//                 [item, el] = [num1, num2]
+//             }
+//         }
+//     }
+//     console.log(arr);
+// }
