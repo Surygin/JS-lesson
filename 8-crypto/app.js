a352567c019365a7cab1d167e7f28a61d76324c9
Anton Surygin <surygin@live.com>
2023-08-22 20:43:24 +0300

    Почистил лишние пробелы

--- 8-crypto/app.js
+++ 8-crypto/app.js
@@ -5,7 +5,7 @@ function crypto(password){
     const arr = password.split('');
     const cryptoPass = [];
     
-    for( i = 0; i < arr.length; i++){
+    for(i = 0; i < arr.length; i++){
         cryptoPass.push(arr[i].charCodeAt(0));
     }
     return cryptoPass;
@@ -15,7 +15,7 @@ function crypto(password){
 function checkPass(cryptoPass, password){
     const arr = [];
 
-    for( i = 0; i < cryptoPass.length; i++){
+    for(i = 0; i < cryptoPass.length; i++){
         arr.push(String.fromCharCode(cryptoPass[i]));
     }
 
