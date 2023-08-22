78ae5ee10311693cf60087cc541984f3bf59ce3a
Anton Surygin <surygin@live.com>
2023-08-22 08:57:47 +0300

    Изменил на if условия

--- 6-driver/app.js
+++ 6-driver/app.js
@@ -1,13 +1,31 @@
-const hasLicense = randomIntFromInterval(0, 1);
-const age = randomIntFromInterval(15, 25);
-const isDrink = randomIntFromInterval(0, 1);
+const hasLicense = false;
+const age = 18;
+const isDrink = false;
+
+if (age >= 18){
+    if (hasLicense === true){
+        if (isDrink === false){
+            console.log('Можно ехать')
+        }
+    }
+    else {
+        console.log('Нельзя за руль')
+    }
+}else {
+    console.log('Нельзя за руль')
+}
+
+
+// const hasLicense = randomIntFromInterval(0, 1);
+// const age = randomIntFromInterval(15, 25);
+// const isDrink = randomIntFromInterval(0, 1);
 
 // функцию нашел в интернете =)
 // с этой функцией - это превращается в приложение для тестирования =) как интересно!
-function randomIntFromInterval(min, max) { // min and max included
-    return Math.floor(Math.random() * (max - min + 1) + min)
-  }
+// function randomIntFromInterval(min, max) { // min and max included
+//     return Math.floor(Math.random() * (max - min + 1) + min)
+//   }
 
-console.log(age >= 18 ? hasLicense ? !isDrink ? 'Можно ехать!' : 'Нельзя за руль, вы пьяны!' : 'Нельзя за руль, нет прав!' : 'Нельзя за руль, вам нет 18!');
+// console.log(age >= 18 ? hasLicense ? !isDrink ? 'Можно ехать!' : 'Нельзя за руль, вы пьяны!' : 'Нельзя за руль, нет прав!' : 'Нельзя за руль, вам нет 18!');
 
 // Прошлая практика, на проверку прав, была сложнее. =)
