163b549d0dca87acffbea278129c05a64aa0e8fd
Anton Surygin <surygin@live.com>
2023-08-24 13:49:01 +0300

    Решил задачу

--- delta-price/app.js
+++ delta-price/app.js
@@ -1,1 +1,5 @@
 const prices = [[100, 200], [120, 100], [200, 350]];
+
+const deltaPrices = prices.map(([oldPrice, newPrice]) => newPrice - oldPrice).filter(delta => delta > 0);
+
+console.log(deltaPrices);
