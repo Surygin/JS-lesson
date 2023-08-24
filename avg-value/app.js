837f33990e9bf470ac2edcc3c126979ea72c35cd
Anton Surygin <surygin@live.com>
2023-08-24 15:17:02 +0300

    решил задачу

--- avg-value/app.js
+++ avg-value/app.js
@@ -1,3 +1,10 @@
 const arr = [1, 4, 4, 10];
 
-const avgValue = arr.reduce(acc, value);
+const avgValue = arr.reduce((acc, value, i) => {
+    if ((i+1) === arr.length){
+        return (acc += value) / arr.length;
+    }
+    return (acc += value);
+}, 0);
+
+console.log(avgValue);
