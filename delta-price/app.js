fde66a8a5899ce10c0987a0d23940ba15857cfe3
Anton Surygin <surygin@live.com>
2023-08-24 15:00:28 +0300

    Удалил лишнее

--- delta-price/app.js
+++
@@ -1,7 +0,0 @@
-const prices = [[100, 200], [120, 100], [200, 350]];
-
-const deltaPrices = prices
-    .map(([oldPrice, newPrice]) => newPrice - oldPrice)
-    .filter(delta => delta > 0);
-
-console.log(deltaPrices);
