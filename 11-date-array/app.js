0c916c69e9ed36cfc4a720190575ae2136d29c47
Anton Surygin <surygin@live.com>
2023-08-26 20:51:29 +0300

    творческий процесс в разгаре

--- 11-date-array/app.js
+++ 11-date-array/app.js
@@ -1,1 +1,12 @@
-const data = ['10-02-2022', 'тест', '11/12/2023', '00/13/2022', '41/12/2023'];
+const data = ['10-02-2022','07-09-1991', 'тест', '11/12/2023', '00/13/2022', '41/12/2023'];
+
+const urls = [];
+const sortArr = data.filter(el => el.length === 10);
+const date1 = sortArr.filter(el => el.split('').find(el => el === '-'));
+const date2 = sortArr.filter(el => el.split('').find(el => el === '/'));
+const date3 = date2.map(el => el.split('/').map(el => console.log(el)));
+console.log(date3);
+// console.log(date1, date2);
+
+// date1.map(el => urls.push(el));
+// console.log(urls)
