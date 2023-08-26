1e854d91d9fc1d0e94035d7e763928ffe7ac2274
Anton Surygin <surygin@live.com>
2023-08-26 21:36:51 +0300

    убрал лишнюю строку с переменными

--- 9-sort-loops/app.js
+++ 9-sort-loops/app.js
@@ -3,9 +3,8 @@ const numbers = [1, 40, -5, 10, 0];
 function sortArray(arr){
     for(let el in arr){
         for (let item in arr){
-            const [num1, num2] = [arr[el], arr[item]];
             if (arr[el] < arr[item]){
-                [arr[item], arr[el]] = [num1, num2]
+                [arr[item], arr[el]] = [arr[el], arr[item]]
             }
         }
     }
