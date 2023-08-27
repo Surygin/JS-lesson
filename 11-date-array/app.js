c649a288f23163e704f37d510f289041b9faed02
Anton Surygin <surygin@live.com>
2023-08-27 22:38:59 +0300

    Как-то работает =)

--- 11-date-array/app.js
+++ 11-date-array/app.js
@@ -2,8 +2,11 @@ const data = ['10-02-2022','07-09-1991', 'тест', 'kjh-d-sf-g', '11/12/2023',
 
 function getDate(urls){
     const sortArr = data.filter(el => el.length === 10);
-    const dmY = sortArr.filter(el => el.split('').find(el => el === '-'));
-    const mdY = sortArr.filter(el => el.split('').find(el => el === '/'));
+    const dmY = sortArr.filter(el => el.split('')
+        .find(el => el === '-'))
+        .filter(el => el.split('-').length === 3);
+    const mdY = sortArr.filter(el => el.split('')
+        .find(el => el === '/'));
     function mdyToDmy(arr){
         arr.map(el => el.split('/')).map(el => {
             [m, d, Y] = el;
@@ -13,10 +16,30 @@ function getDate(urls){
 
     mdyToDmy(mdY);
 
-    console.log(dmY);
+    const trueDMY = dmY.map(el => el.split('-'));
+    // console.log(trueDMY);
+    const sortDate = trueDMY.map(el => el).map(el => {
+        [d, m, Y] = el;
+        if (d > 0 && d < 32){
+            if (m > 0 && m < 13){
+                return [d, m, Y].join('-');
+            }
+        }
+    });
+    return sortDate.filter(el => el != undefined);
 }
 
-getDate(data);
+console.log(getDate(data));
+
+
+// .map(el => {
+//     [d, m, Y] = el;
+//     if (d > 0 && d < 31){
+//         if (m > 0 && m < 32){
+//             console.log(el);
+//         }
+//     }
+// })
 
 // const convertDate = mdY.map(el => el.split('/')).map(el => {
 //     [m, d, Y] = el;
