0fe17a312484df8cc74e93af99bdb14357fb0e45
Anton Surygin <surygin@live.com>
2023-08-30 13:20:01 +0300

    поправил нейминг

--- 9-sort-loops/app.js
+++ 9-sort-loops/app.js
@@ -1,6 +1,6 @@
 const numbers = [1, 40, -5, 10, 0];
 
-function sortArray2(arr){
+function sortArray(arr){
     for(let i = 0; i < arr.length; i++){
         for (let j = 0; j < arr.length; j++){
             if (arr[i] < arr[j]){
@@ -11,4 +11,4 @@ function sortArray2(arr){
     console.log(arr);
 }
 
-sortArray2(numbers);
+sortArray(numbers);
