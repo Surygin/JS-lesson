1070c1e76740a4601fae9e9cdf8ad19bafde2ad2
Anton Surygin <surygin@live.com>
2023-09-04 13:19:28 +0300

    Процесс поиска решения

--- 12-luna/app.js
+++ 12-luna/app.js
@@ -1,1 +1,21 @@
 const card = '4561-2612-1234-5464';
+
+function luna(card){
+    let str = card.replaceAll('-', '');
+    for (i = 0; i < 16; i++){
+        if (i % 2 == 0){
+            if ((str[i] * 2) > 9){
+                // console.log((str[i] * 2) - 9);
+                str[i] = (str[i] * 2) - 9;
+            }else {
+                // console.log(str[i])
+                str[i] = str[i] * 2;
+            }
+        }
+
+
+    }
+    console.log(str);
+};
+
+luna(card);
