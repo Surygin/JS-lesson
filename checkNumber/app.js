123d8dca7834ccf5dea7cf0440b3e2a61b6da00c
Anton Surygin <surygin@live.com>
2023-09-04 09:19:42 +0300

    Начало

---
+++ checkNumber/app.js
@@ -0,0 +1,12 @@
+/* верные */
+const num1 = '89103235356';
+const num2 = '+79103235356';
+const num3 = '+7(910)3235356';
+const num4 = '+7(910) 323-53-56';
+const num5 = ' +7(910) 323-53-56 ';
+/* не верные */
+const num1Error = '89103235';
+const num2Error = '+7d910d323-53-56';
+const num3Error = '9+7103235356';
+const num4Error = '89103g35356';
+
