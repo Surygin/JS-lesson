be9139a3ba5ffeb87882f11b7096e8ebea445e9a
Anton Surygin <surygin@live.com>
2023-09-04 09:48:11 +0300

    Решил задачу

--- checkNumber/app.js
+++ checkNumber/app.js
@@ -10,3 +10,27 @@ const num2Error = '+7d910d323-53-56';
 const num3Error = '9+7103235356';
 const num4Error = '89103g35356';
 
+function clearSpace(phoneNumber){
+    return  phoneNumber.replaceAll(' ', '');
+}
+
+function checkNumber(phoneNumber){
+    const forCheckNumber = [];
+    for (i = 0; i<phoneNumber.length; i++){
+        if (!isNaN(phoneNumber[i])){
+            forCheckNumber.push(phoneNumber[i]);
+        }
+    }
+
+    const trueNumber = forCheckNumber.join('');
+
+    if (trueNumber[0] === '7' || trueNumber[0] === '8'){
+        console.log(trueNumber.length === 11 ? trueNumber : 'не правильный номер');
+    }
+    else {
+        console.log('не правильный номер');
+    }
+
+}
+
+checkNumber(clearSpace(num4Error));
