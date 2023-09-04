36eb364b38c1986ec29068c2271c9cebfeb55b8b
Anton Surygin <surygin@live.com>
2023-09-04 13:47:53 +0300

    Решил

--- 12-luna/app.js
+++ 12-luna/app.js
@@ -1,21 +1,24 @@
 const card = '4561-2612-1234-5464';
 
 function luna(card){
-    let str = card.replaceAll('-', '');
-    for (i = 0; i < 16; i++){
+    let str = card.trim().replaceAll('-', '').split('');
+    for (i = 0; i < str.length; i++){
         if (i % 2 == 0){
             if ((str[i] * 2) > 9){
-                // console.log((str[i] * 2) - 9);
-                str[i] = (str[i] * 2) - 9;
+                // console.log(str[i]);
+                str[i] = Number((str[i] * 2) - 9);
             }else {
                 // console.log(str[i])
-                str[i] = str[i] * 2;
+                str[i] = Number(str[i] * 2);
             }
+        }else {
+            str[i] = Number(str[i]);
         }
-
-
     }
-    console.log(str);
+    const lunaNum = str.reduce((acc, currentValue) => {
+        return acc += currentValue;
+    }, 0);
+    console.log(lunaNum % 10 === 0);
 };
 
 luna(card);
