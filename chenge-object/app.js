8601ca925ff2dd342d630c7a92fbce935a72c2e4
Anton Surygin <surygin@live.com>
2023-09-05 12:50:58 +0300

    Старт проекта

---
+++ chenge-object/app.js
@@ -0,0 +1,13 @@
+const users = [
+    {
+        name: 'Вася',
+        surname: 'Пупкин',
+        age: 30,
+        skills: ['Разработка', 'Dev0ps']
+    },
+    {
+        name: 'Катя',
+        surname: 'Белова',
+        age: 18,
+        skills: ['Дизайн'] },
+];
