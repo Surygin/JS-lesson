5a9a65e206d86b6bf761d7197a213482138cdf05
Anton Surygin <surygin@live.com>
2023-09-06 23:00:33 +0300

    стартовая заготовка

---
+++ warehouse/app.js
@@ -0,0 +1,31 @@
+/*
+Сделать объект склад с методами добавления на склад, поиска по складу товара и расчет веса
+*/
+
+const warehouse = {
+    goods: [],
+    findGoodById: function () {},
+    addGood: function () {},
+    getWeightKg: function () {},
+};
+
+/* Товары */
+const car = {
+    id: 1,
+    weight: {
+        kg: 1000
+    },
+    brand: 'Ford'
+}
+
+const chair = {
+    id: 2,
+    weight: {
+        kg: 2
+    }
+}
+
+const paper = {
+    id: 3,
+    color: 'red'
+}
