04058e7c5cf7b90d4294039b0cc4c6fcf958a278
Anton Surygin <surygin@live.com>
2023-09-15 11:33:42 +0300

    Решил задачу

--- thisPractik/app.js
+++ thisPractik/app.js
@@ -1,13 +1,51 @@
 'use strickt';
 
+/*
+ Дополнить объект методом для получения имени:
+  - компании
+  - сео
+  - сотрудника
+ */
+
 const company = {
     name: 'ООО Агро',
     employees: [
         {
             name: 'Света',
         },
+        {
+            name: 'Дима',
+        },
     ],
     ceo: {
         name: 'Вася'
+    },
+
+    getVseAndSrazu: function (){
+        console.log('Название компании:');
+        console.log(this.name);
+        console.log('Имя CEO компании:');
+        this.ceo.name ? console.log(this.ceo.name) : console.log('Нет такого параметра');
+        console.log('Имена сотрудников:');
+        this.employees.map(el => console.log(el.name));
+
+    },
+
+    getCompanyName: function(){
+        console.log(this.name);
+    },
+
+    getCeoName: function(){
+        this.ceo.name ? console.log(this.ceo.name) : console.log('Нет такого параметра');
+    },
+
+    getEmployeesName: function (){
+        this.employees.map(el => console.log(el.name));
     }
-}
+};
+
+// company.getCompanyName();
+// company.getCeoName();
+// company.getEmployeesName();
+
+company.getVseAndSrazu();
