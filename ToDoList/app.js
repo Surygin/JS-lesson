3a6d9bae603351f1f3e9bf75f91901880136b84b
Anton Surygin <surygin@live.com>
2023-09-16 12:31:13 +0300

    реализовал добавление и удаление задачи

--- ToDoList/app.js
+++ ToDoList/app.js
@@ -1,12 +1,35 @@
 'use strict';
 
-const   ToDoList = {
+const ToDoList = {
 
-  taskList: {
-
-  },
+  taskList: [],
 
   addTask: function (name, priority){
-    this.taskList.push();
+    this.taskList.push({
+      id: this.taskList.length < 1 ? 1 : this.taskList[this.taskList.length - 1].id + 1,
+      name: name,
+      priority: priority,
+    });
+  },
+  howMatch: function (){ //метод для отладки - после удалить
+    console.log(this.taskList.length);
+  },
+  removeTask: function (id){
+    this.taskList.map(el => {
+      if (el.id === id){
+        this.taskList.splice((id - 1), id);
+      }
+    });
   },
 };
+
+ToDoList.addTask('Написать задачу', 1);
+ToDoList.addTask('накормить кота', 2);
+
+ToDoList.removeTask(1);
+ToDoList.addTask('Написать задачу', 1);
+ToDoList.addTask('Написать задачу', 1);
+
+ToDoList.howMatch();
+
+ToDoList.taskList.map(el => console.log(el));
